145dc4e9c9fa3b3316c910e47e00ed598f579c44
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-14 15:34:02 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,17 +1,5 @@
 require_relative './spec_helper.rb'
-def fizzbuzz(int)
-  if int % 3 == 0
-return "fizz"
-end
-
-if int % 5 == 0 
-  return "buzz"
-end
-  
-if int % 3 == 0 && if int % 5 == 0 
-  return "fizzbuzz"
-end
-end
+require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
